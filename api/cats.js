edf036f1a13c820e7c908cd6676da99edad26a0d
blueberryenjoyer <phoenix.wheatley@gmail.com>
2023-07-07 16:39:11 -0400

    turned on the cats menu as well

--- api/cats.js
+++ api/cats.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const Router = express.Router();
-//const { getAllCats } = require("../db/getData");
+const { getAllCats } = require("../db/getData");
 
 Router.get("/", async (req, res) => { //working, thanks ed
-    res.send('cats test response')
 
-    // try {
-    //     const allCats = await getAllCats();
-    //     res.send(allCats);
+    try {
+        const allCats = await getAllCats();
+        res.send(allCats);
 
-    // } catch (error) {
-    //     throw error;
-    // }
+    } catch (error) {
+        throw error;
+    }
 });
 
 
